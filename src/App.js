c724045c3546a99f54511e1f6da156b0f90e1a70
smellyung <kelly.ung@centrica.com>
2019-09-23 14:17:26 +0100

    smelly kelly

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
+        <h1>
+          This is the cool feature!!
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
